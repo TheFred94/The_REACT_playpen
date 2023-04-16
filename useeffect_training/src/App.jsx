e0ef626db851fb063da04fa8d716c318b8df4e47
TheFred94 <frederikrlarsen@gmail.com>
2023-04-16 13:59:11 +0200

    short circuit evaluation shows on sale now when product is discountet

--- useeffect_training/src/App.jsx
+++ useeffect_training/src/App.jsx
@@ -3,20 +3,29 @@ import { useState, useEffect } from "react";
 import "./App.css";
 
 function App() {
+  //  Creates state and set it to []
   const [articles, setArticles] = useState([]);
 
+  //  Uses an arrow function for the callback
   useEffect(() => {
     fetch("https://kea-alt-del.dk/t7/api/products")
       .then((res) => res.json())
       .then((data) => {
+        // set state which triggers the render again
         setArticles(data);
       });
+    // provides an empty array so it runs once
   }, []);
 
   return (
+    // This one is automatic
     <ul>
       {articles.map((art) => (
-        <li>{art.productdisplayname}</li>
+        <li>
+          <article>{art.productdisplayname}</article>
+          {/* Conditional rendering (Short curcuit evaluation) - If the product has a discount it shows "On sale now!"  */}
+          {art.discount && <p>On sale now!</p>}
+        </li>
       ))}
     </ul>
   );
