619eac4420e1f86094944a9ef717a6debb3ca086
TheFred94 <frederikrlarsen@gmail.com>
2023-04-16 14:16:09 +0200

    Buy product button have been implementet

--- useeffect_training/src/App.jsx
+++ useeffect_training/src/App.jsx
@@ -5,17 +5,18 @@ import "./App.css";
 function App() {
   //  Creates state and set it to []
   const [articles, setArticles] = useState([]);
+  const [page, setPage] = useState(0);
 
   //  Uses an arrow function for the callback
   useEffect(() => {
-    fetch("https://kea-alt-del.dk/t7/api/products")
+    fetch("https://kea-alt-del.dk/t7/api/products?start=" + page * 10)
       .then((res) => res.json())
       .then((data) => {
         // set state which triggers the render again
         setArticles(data);
       });
-    // provides an empty array so it runs once
-  }, []);
+    // Runs this useEffect when the page is loaded and whenever it changes
+  }, [page]);
 
   return (
     <>
@@ -29,11 +30,13 @@ function App() {
               <article>{art.productdisplayname}</article>
               {/* Conditional rendering (Short curcuit evaluation) - If the product has a discount it shows "On sale now!"  */}
               {art.discount && <p>On sale now!</p>}
+              <button>Buy product</button>
             </li>
           ))}
         </ul>
       )}
-      {/* This one is automatic */}
+      {/* Whenever we click it updates the pagecounter and sends a new request because we told the useEffect Hook to look out for changes in page  */}
+      <button onClick={() => setPage((oldPage) => oldPage + 1)}>Load 10 more products({page})</button>
     </>
   );
 }
