2d2bdbf776a15d213c84ea42ca75a141d0403f21
TheFred94 <frederikrlarsen@gmail.com>
2023-04-16 12:34:22 +0200

    filtering works with little code and a lot of magic

--- mini-animal-base/src/App.jsx
+++ mini-animal-base/src/App.jsx
@@ -14,14 +14,28 @@ function cleanUpData() {
     };
   });
 }
+
 import "./App.css";
 
 function App() {
   const animals = cleanUpData();
-  console.log(animals);
+  // We create a state with a function that is called filter which runs setFilter when the state changes
+  const [filter, setFilter] = useState("");
+
+  // We create a variable that contain everything
+  let filteredAnimals = [...animals];
+  // if there is a state we override filteredAnimals and pull out only the ones that matches the filter.
+  if (filter) {
+    filteredAnimals = filteredAnimals.filter((ani) => ani.type === filter);
+  }
   return (
     <div className="App">
       <h1>Mini animal base</h1>
+      {/* onClick runs the setFilter function which determines what the user filters on */}
+      <button onClick={() => setFilter("cat")}>Only cats</button>
+      <button onClick={() => setFilter("dog")}>Only dogs</button>
+      <button onClick={() => setFilter("")}>All</button>
+
       <table>
         <thead>
           <tr>
@@ -33,7 +47,8 @@ function App() {
         </thead>
         {/* The table is filled with the data from the json file trough the variables name, trait, type and age trough the animal. variable*/}
         <tbody>
-          {animals.map((animal) => (
+          {/* Here we map through the copy of the animals array instead of the original */}
+          {filteredAnimals.map((animal) => (
             <tr>
               <td>{animal.name}</td>
               <td>{animal.trait}</td>
