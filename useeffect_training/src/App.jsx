4ab0b02ffc89987529ca2e14b05ef6638ffb78d9
TheFred94 <frederikrlarsen@gmail.com>
2023-04-16 19:03:26 +0200

    products showin with unique key

--- useeffect_training/src/App.jsx
+++ useeffect_training/src/App.jsx
@@ -6,7 +6,6 @@ function App() {
   //  Creates state and set it to []
   const [articles, setArticles] = useState([]);
   const [page, setPage] = useState(0);
-
   //  Uses an arrow function for the callback
   useEffect(() => {
     fetch("https://kea-alt-del.dk/t7/api/products?start=" + page * 10)
@@ -14,6 +13,7 @@ function App() {
       .then((data) => {
         // set state which triggers the render again
         setArticles(data);
+        console.log(data);
       });
     // Runs this useEffect when the page is loaded and whenever it changes
   }, [page]);
@@ -25,19 +25,34 @@ function App() {
         <p>LOADING...</p>
       ) : (
         <ul>
-          {articles.map((art) => (
-            <li>
-              <article>{art.productdisplayname}</article>
-              {/* Conditional rendering (Short curcuit evaluation) - If the product has a discount it shows "On sale now!"  */}
-              {art.discount && <p>On sale now!</p>}
-              <button>Buy product</button>
-            </li>
+          {/* Creating the component Product with functions and passing it down to the Product component */}
+          {articles.map((article) => (
+            <Product key={article.id} product={article} buyProduct={buyProduct} />
           ))}
         </ul>
       )}
       {/* Whenever we click it updates the pagecounter and sends a new request because we told the useEffect Hook to look out for changes in page  */}
       <button onClick={() => setPage((oldPage) => oldPage + 1)}>Load 10 more products({page})</button>
     </>
+  );
+
+  function buyProduct(id) {
+    console.log("clicked product");
+  }
+}
+
+function ProductList({ product, buyProduct }) {}
+
+function Product({ product, buyProduct }) {
+  return (
+    <li key={product.id}>
+      <article>
+        <h2>{product.productdisplayname}</h2>
+        <button onClick={buyProduct}>Buy product</button>
+      </article>
+      {/* Conditional rendering (Short curcuit evaluation) - If the product has a discount it shows "On sale now!"  */}
+      {product.discount && <p>On sale now!</p>}
+    </li>
   );
 }
 
