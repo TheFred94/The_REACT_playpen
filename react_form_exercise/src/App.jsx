f2d9d773b531d5431a6bf49e9ba842cddbe78ec6
TheFred94 <frederikrlarsen@gmail.com>
2023-04-16 10:34:26 +0200

    Each individual task from the array objects are shown. Comments are added for context and understanding.

--- react_form_exercise/src/App.jsx
+++ react_form_exercise/src/App.jsx
@@ -3,12 +3,32 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-
+  // Create an array with tasks that contains an object for each task. This is the initial state that is passed down to <List />
+  const [tasks, setTasks] = useState([
+    {
+      task: "First task",
+      completed: false,
+      id: 1,
+    },
+    {
+      task: "Second task",
+      completed: false,
+      id: 2,
+    },
+    {
+      task: "Third task",
+      completed: false,
+      id: 3,
+    },
+  ]);
+  const handler = () => {
+    console.log("handler called");
+  };
   return (
     <div className="App">
-      <Form />
-      <List />
+      <Form clickHandler={handler} />
+      {/* List gets the initial state from the const [tasks, setTasks]*/}
+      <List tasks={tasks} />
     </div>
   );
 }
@@ -17,28 +37,35 @@ function Form() {
   return (
     <>
       <input></input>
-      <Button />
+      <button>Add Task</button>
     </>
   );
 }
 
-function List() {
+// For list to pick up the state tasks we pass in props
+function List(props) {
   return (
-    <>
-      <article>Some list</article>
-      <ListItem />
-    </>
+    // We use map to map through the tasks array and create a ListItem for each of the tasks with their respective properties
+    <ul>
+      <p>Some List</p>
+      {props.tasks.map((task) => (
+        // To show each task in as their own element we use the ... spread operator to devide the array into meaningfull tasks
+        <ListItem {...task} />
+      ))}
+    </ul>
   );
 }
 
-function ListItem() {
+// ListItem is passed in props since we have used the spread operator ...task in on the <ListItem /> in the <List /> component
+function ListItem(props) {
   return (
-    <>
-      <article>sometask</article>
-      <section>
-        <Button />
-      </section>
-    </>
+    <li>
+      <article>
+        {/* Using the {props.task} then gets each individual task from the ListItem because we used the spreap operator ...task on the ListItem.   */}
+        <p>{props.task}</p>
+        <button>Complete</button>
+      </article>
+    </li>
   );
 }
 
