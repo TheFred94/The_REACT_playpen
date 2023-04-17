4aa1d4e7aa0853e5bed635c7eb96e55b1881ccda
TheFred94 <frederikrlarsen@gmail.com>
2023-04-17 14:32:17 +0200

    random dogs on load

--- working-with-nextjs/src/pages/dogs/henry.jsx
+++ working-with-nextjs/src/pages/dogs/henry.jsx
@@ -1,3 +1,10 @@
+import Image from "next/image";
+
 export default function Henry() {
-  return <h1>Henry page</h1>;
+  return (
+    <>
+      <h1>Henry page</h1>
+      <Image src="https://images.unsplash.com/photo-1583511655857-d19b40a7a54e" alt="Cute Dog" width="4240" height="2823" sizes="(max-width: 750px) 100vw, 750px" />
+    </>
+  );
 }
---
+++ working-with-nextjs/src/pages/dogs/random.jsx
@@ -0,0 +1,23 @@
+export default function RandomDog({ imgUrl }) {
+  console.log(imgUrl);
+  return (
+    <>
+      <h1>Random dog</h1>
+      <img src={imgUrl} alt="Random dog" />
+    </>
+  );
+}
+
+// BACKEND
+export async function getServerSideProps() {
+  const api = "https://dog.ceo/api/breeds/image/random";
+  const res = await fetch(api);
+  const data = await res.json();
+  console.log(data);
+
+  return {
+    props: {
+      imgUrl: data.message,
+    },
+  };
+}
