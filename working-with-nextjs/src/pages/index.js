510464f955051b1f9d4af13c802f0d4cfe0f5481
TheFred94 <frederikrlarsen@gmail.com>
2023-04-17 14:33:23 +0200

    random dogs on load of the page

--- working-with-nextjs/src/pages/index.js
+++ working-with-nextjs/src/pages/index.js
@@ -1,7 +1,14 @@
+import Head from "next/head";
+import Anchor from "@/components/Anchor";
+
 export default function Home() {
   return (
     <>
+      <Head>
+        <title>Welcome to a random dog page</title>
+      </Head>
       <h1>Hello from home</h1>
+      <Anchor href="/dogs/henry">Henry</Anchor>
     </>
   );
 }
