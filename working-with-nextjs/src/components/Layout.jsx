bbadec8b6f66ce5fcfc9483f03ea991d217c62fe
TheFred94 <frederikrlarsen@gmail.com>
2023-04-18 12:28:02 +0200

    get initial prods

--- working-with-nextjs/src/components/Layout.jsx
+++ working-with-nextjs/src/components/Layout.jsx
@@ -1,7 +1,14 @@
-export default function Layout({ children }) {
+import Anchor from "./Anchor";
+
+export default function Layout({ children, navData }) {
+  console.log(navData);
   return (
     <>
-      <nav>Navigation</nav>
+      <nav>
+        {navData.map((obj) => {
+          return <Anchor href={"/dogs/" + obj.slug}>{obj.title}</Anchor>;
+        })}
+      </nav>
       {children}
       <footer>Footer</footer>
     </>
