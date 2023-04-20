3bae11d09f7f4435dca305f24809d2b7a51548d7
TheFred94 <frederikrlarsen@gmail.com>
2023-04-20 17:14:18 +0200

    shows the text after 2 sek and text replaces the skeleton loader

--- ReactFlipCard/src/App.jsx
+++ ReactFlipCard/src/App.jsx
@@ -1,17 +1,39 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import ReactFlipCard from "@holbech/react-flip-card";
-
+import Skeleton from "react-loading-skeleton";
+import "react-loading-skeleton/dist/skeleton.css";
 import "./App.css";
 
 function App() {
+  const [paragraph, setShowParagraph] = useState(false);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setShowParagraph(true);
+    }, 2000);
+    return () => {
+      clearTimeout(timer);
+    };
+  }, []);
+  console.log(paragraph);
+
   return (
     <div className="App">
-      <div style={{ "--width": "500px", "--height": "500px" }}>
-        <ReactFlipCard>
-          <p>Front</p>
-          <p>Back</p>
-        </ReactFlipCard>
-      </div>
+      <>
+        <div style={{ "--width": "500px", "--height": "500px" }}>
+          <ReactFlipCard>
+            <p>Front</p>
+            <p>Back</p>
+          </ReactFlipCard>
+        </div>
+        {paragraph ? (
+          <div>
+            <p>Damn the site loaded!</p>
+          </div>
+        ) : (
+          <Skeleton />
+        )}
+      </>
     </div>
   );
 }
